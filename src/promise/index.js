bcaab5eb51e7c299cb7523868dfa3c7763aebe00
rage875 <LLopez@luxoft.com>
2021-05-20 13:43:40 -0500

    Promise: promise foo time and Promise all example

--- src/promise/index.js
+++ src/promise/index.js
@@ -8,6 +8,32 @@ const promiseFoo = () => {
     });
 };
 
+const promiseFooTimeOut = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => {
+                resolve("Promise Resolve from foo timeout called");
+            },
+            2000);
+        } else {
+            reject("Promise Reject Called");
+        }
+    });
+};
+
 promiseFoo()
     .then(response => console.log(response))
     .catch(response => console.log(response));
+
+promiseFooTimeOut()
+    .then(response => console.log(response))
+    .catch(response => console.log(response));
+
+// To catch different promises result
+Promise.all([promiseFoo(), promiseFooTimeOut()])
+    .then( reponse => {
+        console.log(`Array of results: ${reponse}`)
+    })
+    .catch( error => {
+        console.log(`Array of errors: ${error}`)
+    });
